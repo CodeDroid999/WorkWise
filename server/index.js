13a91c055489d2fd3952bb0e6ed53698cad991c5
CodeDroid99 <johnmwega2002@gmail.com>
2023-07-19 17:33:20 +0300

    Server is running at http://localhost:3001

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.listen(port,()=>{
-    console.log('Server is running on port: http://localhost:${port}')
-})
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
