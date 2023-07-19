5d6af205e19e74453f7af90f18c80e333a1262da
CodeDroid99 <johnmwega2002@gmail.com>
2023-07-19 17:51:32 +0300

    use cors for resouryce sharing

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+
 dotenv.config();
 
 const app = express();
@@ -10,8 +11,14 @@ const port = process.env.PORT;
 app.use(
   cors({
     origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
   })
 );
+
+app.use(express.json());
+
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
