98667f35370ecc2acf73af54791a6a4984c1cb76
CodeDroid99 <johnmwega2002@gmail.com>
2023-07-19 17:40:52 +0300

    add public url

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+  })
+);
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-  });
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
